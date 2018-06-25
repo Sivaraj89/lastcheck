01e6021832617de93a7fe3971b1cb1c29d48970c
Sivaraj Selvaraj <sivarajselvaraj@Sivarajs-MacBook-Pro.local>
2018-06-25 20:31:48 +0530

    ssss

---
+++ checkingFinal/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  checkingFinal
+//
+//  Created by Sivaraj Selvaraj on 25/06/18.
+//  Copyright © 2018 Sivaraj Selvaraj. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
